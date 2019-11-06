9df3181f30397df3cd39fd2648fb8dbc723740e1
kangsm0903 <kangsm0903@naver.com>
2019-11-06 22:06:44 +0900

    third commit

--- algo.py
+++ algo.py
@@ -73,13 +73,35 @@
 
 # python 2588번
 # 값들을 list에 넣고 해야할 듯 
-a = list(input())
-b = list(input())
+# a = list(input())
+# b = list(input())
 
-a.append(a[0]+a[1]+a[2])
-b.append(b[0]+b[1]+b[2])
+# a.append(a[0]+a[1]+a[2])
+# b.append(b[0]+b[1]+b[2])
 
-print(int(a[3])*int(b[2]))
-print(int(a[3])*int(b[1]))
-print(int(a[3])*int(b[0]))
-print(int(a[3])*int(b[3]))
+# print(int(a[3])*int(b[2]))
+# print(int(a[3])*int(b[1]))
+# print(int(a[3])*int(b[0]))
+# print(int(a[3])*int(b[3]))
+
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# 10869번
+# a = list(input().split())
+
+# # a = list(map(int,a))
+
+# for A in a[0]:
+#     for B in a[1]:
+#         print(int(A) + int(B))
+#         print(int(A) - int(B))
+#         print(int(A) * int(B))
+#         print(int(A) // int(B))
+#         print(int(A) % int(B))
+
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+print(a+b, a-b, a*b, a//b, a%b)
