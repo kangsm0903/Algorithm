bfff91474ebed9d428ec80a4ec751a7bf7bf8777
kangsm0903 <kangsm0903@naver.com>
2019-11-17 23:43:51 +0900

    27 Algorithm

--- B_2000~/B_2920.py
+++ B_2000~/B_2920.py
@@ -10,3 +10,18 @@ else:
     print('mixed')
 
 
+# 모범답안
+# if A ==  sorted(A):
+#     print('ascending')
+# elif A == sorted(A, reverse=True):
+#     print('descending')
+# else:
+#     print('mixed')
+
+#다른 풀이
+# if A == [1,2,3,4,5,6,7,8]:
+#     print('ascending')
+# elif A == [8,7,6,5,4,3,2,1]:
+#     print('descending')
+# else:
+#     print('mixed')
