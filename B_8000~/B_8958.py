91ac60349c07c6d88691bfbd64aa9aa42beac9fa
kangsm0903 <kangsm0903@naver.com>
2019-11-23 20:13:30 +0900

    32 Algorithm

--- B_8000~/B_8958.py
+++ B_8000~/B_8958.py
@@ -7,17 +7,26 @@ A = {}
 for i in range(0,N):
     A[i] = input().split('X')
 
-print(A)
-
-B = []
 C = []
 
 for i in range(0,N):
+    C.append([])
     for k in range(0,len(A[i])):
-        P = len(A[i][k])
-        B.append(P*(P+1)/2)
-    C.append(B)
-        
+        B = len(A[i][k])
+        C[i].append((B*(B+1)/2)) # 시그마 n 을 사용함 
 
-print(B)
-print(C)    
+for i in range(0,N):
+    print(int(sum(C[i])))
+
+# 하나의 key값에 다수의 value값을 넣어볼라고 setdefault, default를 써봤지만 좀더 공부해야할 듯
+# B = {}
+
+# for i in range(0,N):
+#     for k in range(0,len(A[i])):
+#         P = len(A[i][k])
+#         if i in B:
+#             B.setdefault(i,[]).append(P(P+1)/2)
+#         else:
+#             B[i] = (P*(P+1)/2)
+        
+# print(B) 
