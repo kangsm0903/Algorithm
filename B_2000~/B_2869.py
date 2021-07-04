44712c55b2b15928e0e60b188c231c2a34054ef3
kangsm0903 <kangsm0903@naver.com>
2021-07-04 11:16:46 +0900

    75 Algorithm

--- B_2000~/B_2869.py
+++ B_2000~/B_2869.py
@@ -1,22 +1,21 @@
 # 2021/07/02 2869 달팽이는 올라가고 싶다
 # 시간초과로 인한 실패..
 
+import math
+
 N = list(map(int, input().split()))
 
 A = N[0] # up 
 B = N[1] # down
 V = N[2] # length
 
-count = 0
-height = 0
+day = (V-B)/(A-B)
 
-day = 0
+print(math.ceil(day))
 
-while True:
-    height += A
-    count += 1
-    if height >= V:
-        break
-    height -= B
-
-print(count)
+# while True:
+#     height += A
+#     count += 1
+#     if height >= V:
+#         break
+#     height -= B
