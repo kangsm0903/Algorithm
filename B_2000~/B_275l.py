8e47399c1cb23c3c9ac6331b5d2eb4231d3600c7
kangsm0903 <kangsm0903@naver.com>
2021-07-06 21:45:29 +0900

    69 Algorithm

--- B_2000~/B_275l.py
+++ B_2000~/B_275l.py
@@ -1,11 +1,18 @@
 # 2020/01/10 2751번 pypy3로 제출
-
+# 2021/07/06
 N = int(input())
 
-M = []
+L = [ int(input()) for i in range(0,N) ]
 
-for i in range(0, N):
-    M.append(int(input()))
+A = [ print(k) for k in sorted(L)]
+    
+#-----------------------------------------------------------------------------------------------------------
+# N = int(input())
 
-for i in sorted(M):
-    print(i)
+# M = []
+
+# for i in range(0, N):
+#     M.append(int(input()))
+
+# for i in sorted(M):
+#     print(i)
