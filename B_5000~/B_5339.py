18f6a86c551b385e08e2363c0db632c4477fc1f9
kangsm0903 <kangsm0903@naver.com>
2021-07-11 15:16:16 +0900

    85 Algorithm

---
+++ B_5000~/B_5339.py
@@ -0,0 +1,22 @@
+A = "     /~\\"
+B = "    ( oo|"
+C = "    _\=/_"
+D = "   /  _  \\"
+E = "  //|/.\|\\\\"
+F = " ||  \ /  ||"
+G = "============"
+H = "|          |"
+I = "|          |"
+J = "|          |"
+
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+print(F)
+print(G)
+print(H)
+print(I)
+print(J)
+
