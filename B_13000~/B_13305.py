c7dbefa60ffee5594f32f86861c6517020009f92
kangsm0903 <kangsm0903@naver.com>
2022-02-06 21:20:31 +0900

    Greedy Algorithm 4

--- B_13000~/B_13305.py
+++ B_13000~/B_13305.py
@@ -1,22 +1,45 @@
-# 58점 O(n^2) 시간복잡도를 고려해서 다시 작성해볼 것
+#O(n) 시간복잡도를 고려해서 작성함 100점
 N=int(input()) # 5
-road=list(map(int, input().split( ))) # [5,3,2,1]
-city=list(map(int, input().split( ))) # [3,4,2,2,3]
+distance=list(map(int, input().split( ))) # [5,3,2,1]
+price=list(map(int, input().split( ))) # [3,4,2,2,3]
 
-pay=[]
-while len(city)!=0:
-    total=0
-    A=city.index(min(city)) # 2, 0
+total = price[0]*distance[0]
+mprice=price[0]
 
-    for i in range(A,len(road)): # 최솟값을 구하고 그 뒤에 도로거리들의 합
-        total+=road[i] # total = 2 + 1
+for i in range(N-1): # 0,1,2
+    try:
+        if mprice <= price[i+1]:    # 이전 값이 더 작을 때
+            pass
+        else:                           # 이후 값이 더 작을 때
+            mprice=price[i+1]
+        total = total + mprice*distance[i+1]
+    except IndexError:
+        print(total)
 
-    pay.append(total*city[A]) # 최솟값 기준 뒤의 도로거리 * 최솟값 
 
-    for i in range(A,len(city)): # 최솟값 포함해서 뒤에 값들 삭제
-        del city[A]
-
-    for i in range(A,len(road)):
-        del road[A]
+# 최솟값이 갱신되는 것을 고려하지않았음. 17점
+total = price[0]*distance[0]
+for i in range(N-1):
+    try:
+        if price[i]<=price[i+1]:
+            total = total + price[i]*distance[i+1]
+        elif price[i]>price[i+1]:
+            total = total + price[i+1]*distance[i+1]
+    except IndexError:
+        print(total)
     
+# while 때문에 시간이 많이 걸리는 것으로 보임
+pay=[]
+while len(price)!=0:
+    A=price.index(min(price)) # 2, 0
+
+    # 최솟값을 구하고 그 뒤에 도로거리들의 합
+    total = sum(distance[A:])
+
+    pay.append(total*price[A]) # 최솟값 기준 뒤의 도로거리 * 최솟값 
+
+    # 최솟값 포함해서 뒤에 값들 삭제
+    del price[A:]
+    del distance[A:]
+
 print(sum(pay))
