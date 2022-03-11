c3d7ed320fd496511f944eea9883278430c8aef8
kangsm0903 <kangsm0903@naver.com>
2022-03-11 15:53:06 +0900

    Greedy Algorithm 29

--- B_13000~/B_13904.py
+++ B_13000~/B_13904.py
@@ -1,3 +1,44 @@
+# 아이디어만 참조했슴..ㅠ
+# 마감일 높은 기준으로 정렬 [6,5][4,60][4,40][4,10][3,30][2,50][1,20]
+# 6일차부터 내려가면서 수행할 수 있는 과제의 최대값을 진행 = 거꾸로 내려가는 방식
+
+# 6일차 - 유일하게 할 수 있는 [6,5] 진행
+# 5일차 - 진행할 수 있는 과제가 없음
+# 4일차 - [4,60][4,40][4,10] 중 최대값인 [4,60] 진행
+# 3일차 - [4,40][4,10][3,30] 중 최대값인 [4,40] 진행
+# 2일차 - [4,10][3,30][2,50] 중 최대값인 [2,50] 진행
+# 1일차 - [4,10][3,30][1,20] 중 최대값인 [3,30] 진행
+
+# filter(lambda x:x[0]>=i,case)를 이용하여 n일차일 때 n일차보다 과제마감일이 같거나 큰 것을 필터링함
+import sys
+
+N=int(sys.stdin.readline())
+
+case=[]
+
+for i in range(N):
+    case.append(list(map(int, sys.stdin.readline().split())))
+
+case.sort(reverse=True) # 마감일 순으로 정렬
+
+day=max(case, key=lambda x:x[0])[0] # case 과제 마감일의 최대값을 day에 할당
+sub=[]
+total=[]
+
+for i in range(day,0,-1):
+    for k in filter(lambda x:x[0]>=i,case): # n일차보다 과제마감일이 같거나 큰 것들을 필터링하여 sub에 넣음
+        sub.append(k)
+    try:
+        total.append(max(sub, key=lambda x:x[1])[1]) # sub에서 점수가 제일 높은 것을 total에 넣어줌
+        case[case.index(max(sub, key=lambda x:x[1]))]=[0,0] # total에 할당된 값을 [0,0]으로 바꿔서 인덱스 값을 유지시킴
+    except ValueError: # 5일차에 진행할 수 있는 과제가 없는 거처럼 sub에 값이 없어 ValueError가 발생하면 pass
+        pass
+    sub=[]
+
+print(sum(total))
+
+#-------------------------------------------------------------------------------------------------
+
 # 오늘 날짜 day를 설정
 # n일차 점수 최대값 - 전체의 최대값
 # n일차 점수 최대값 > 전체의 최대값이라면 n일차 점수 최대값을 넣음
