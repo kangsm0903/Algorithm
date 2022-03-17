7b8bbb661082b7d42dec52dc8cf771ae49780d93
kangsm0903 <kangsm0903@naver.com>
2022-03-17 23:06:11 +0900

    Linked List #2

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -1,3 +1,68 @@
+class SList:
+
+    class Node:
+        def __init__(self, item, link): # 노드 생성자
+            self.item=item # element
+            self.next=link # 다음 노드 레퍼런스
+
+    def __init__(self): # 단순 연결 리스트 생성자
+        self.head=None
+        self.size=0
+
+    def size(self): # 노드 사이즈 반환
+        return self.size
+
+    def is_empty(self): 
+        return self.size==0
+
+    def insert_front(self,item): # 맨 앞에 삽입
+        if self.is_empty(): # 비어있다면 새 노드를 head로 지정
+            self.head=self.Node(item,None)
+        else: # 새 노드가 head의 레퍼런스를 가리키고 이 노드를 head로 지정
+            self.head=self.Node(item,self.head)
+        self.size+=1
+
+    def insert_after(self,item,p): # p 다음에 삽입
+        p.next=self.Node(item,p.next) # 새 노드를 만들고 p.next 레퍼런스를 가리키고 이 노드를 p의 다음으로 설정
+        self.size+=1
+
+    def delete_front(self): # 맨 앞 삭제
+        if self.is_empty(): # 비어있다면 에러 출력
+            raise EmptyError('Underflow')
+        else: # 맨 앞 다음의 노드를 head로 지정
+            self.head=self.head.next
+            self.size-=1
+        
+    def delete_after(self,p): # p 다음 노드 삭제
+        if self.is_empty(): # 비어있다면 에러 출력
+            raise EmptyError('Underflow')
+        t=p.next # t에 p.next 레퍼런스를 할당
+        p.next=t.next # p.next.next 레퍼런스를 p.next에 할당 
+        # A -> B -> C 일 때 p.next = B를 p.next.next = C 로 할당해서 B를 삭제한 것
+        self.size+=1
+
+    def search(self,target):
+        p=self.head
+        for i in range(self.size):
+            if target==p.item:
+                return i
+            p=p.next
+        return None # 탐색 실패
+
+    def print_list(self):
+        p=self.head
+        while p:
+            if p.next != None:
+                print(p.item, "->", end="")
+            else:
+                print(p.item)
+        p=p.next # 노드들을 순차탐색
+
+class EmptyError(Exception): # 에러처리
+    pass
+
+# ------------------------------------------------------------------------------------------
+
 # Node 클래스 정의
 class Node:
     def __init__(self,data): # 생성자
@@ -53,4 +118,3 @@ class LinkedList:
 
         # [5]->[12]->[8]  ----[6]----
         # 삭제하려는 [6]의 전 노드 [5]를 할당하고 [5]의 next.next와 연결
-  
