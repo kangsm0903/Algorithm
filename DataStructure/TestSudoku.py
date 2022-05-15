878104d3ee10cf1d41b2715a2373fb1b3d679ece
kangsm0903 <kangsm0903@naver.com>
2022-05-15 21:00:21 +0900

    Unittest Sudoku +

--- DataStructure/TestSudoku.py
+++ DataStructure/TestSudoku.py
@@ -2,18 +2,21 @@ import unittest
 import Sudoku
 
 class TestSudoku(unittest.TestCase):
-    
+    # unittest
     def test_1(self):
-        self.assertEqual(Sudoku.solveSudoku2by2([[0,2,1,3], [0,0,0,0], [0,0,0,0], [2,1,3,0]]),[[4,2,1,3], [1,3,4,2], [3,4,2,1], [2,1,3,4]])
+        a=[[0,2,1,3], [0,0,0,0], [0,0,0,0], [2,1,3,0]]
+        last=[[4,2,1,3], [1,3,4,2], [3,4,2,1], [2,1,3,4]]
+        self.assertListEqual(Sudoku.solveSudoku2by2(a),last)
 
     def test_2(self):
-        self.assertEqual(Sudoku.solveSudoku2by2([[2,3,1,4], [1,2,4,0], [3,4,0,2], [0,0,0,1]]),"Impossible")
+        a=[[0,3,1,2], [0,0,0,0], [0,0,0,0], [2,1,3,0]]
+        last=[[4,3,1,2], [1,2,4,3], [3,4,2,1], [2,1,3,4]]
+        self.assertListEqual(Sudoku.solveSudoku2by2(a),last)
 
     def test_3(self):
-        self.assertEqual(Sudoku.solveSudoku2by2([[0,3,1,2], [0,0,0,0], [0,0,0,0], [2,1,3,0]]),[[4,3,1,2], [1,2,4,3], [3,4,2,1], [2,1,3,4]])
-
-    def test_4(self):
-        self.assertEqual(Sudoku.solveSudoku2by2([[0,3,1,2], [0,0,0,0], [0,0,0,0], [2,1,3,0]]),[[4,3,1,2], [1,2,4,3], [3,4,2,1], [2,1,3,4]])
-
+        a=[[2,3,1,4], [1,2,4,0], [3,4,0,2], [0,0,0,1]]
+        last='Impossible'
+        self.assertEqual(Sudoku.solveSudoku2by2(a),last)
+    
 if __name__ == '__main__': 
     unittest.main()
