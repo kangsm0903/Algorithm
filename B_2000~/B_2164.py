4fbaf34cacbb4459abac727a7d7c5c26a5b2334d
kangsm0903 <kangsm0903@naver.com>
2022-07-08 15:11:28 +0900

    Queue Algorithm

--- B_2000~/B_2164.py
+++ B_2000~/B_2164.py
@@ -22,3 +22,74 @@ for i in range(N-1):
     case.append(item)
 
 print(case[0])
+
+# ------------------------------------------------------------------
+# 단일 연결리스트를 통해 Queue 구현 후 풀음
+
+import sys
+
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+
+class queue():
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def size(self):
+        return self.length
+
+    def push(self,A):
+        new_node=Node(A) # create new_node
+
+        if self.head==None: # if head is not exist, insert new_node
+            self.head=new_node
+            self.tail=self.head
+            self.length+=1
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+            self.length+=1
+
+    def pop(self):
+        if self.length!=0: # if length is not equal 0, keep going pop method
+            node=self.head
+            self.head=self.head.next
+            self.length-=1
+            return node.data
+        else:
+            return -1
+
+    def empty(self):
+        if self.length==0:
+            return 1
+        else:
+            return 0
+
+    def front(self):
+        if self.length==0:
+            return -1
+        else:
+            return self.head.data
+    
+    def back(self):
+        if self.length==0:
+            return -1
+        else:
+            return self.tail.data
+
+N=int(sys.stdin.readline())
+
+case=queue()
+
+for i in range(N):
+    case.push(i+1) # [1,2,3,4,5,6]
+
+while case.size()!=1:
+    case.pop()
+    case.push(case.pop())
+
+print(case.front())
