552d975c8631fdd5f0ac3c8f72a313416739da45
kangsm0903 <kangsm0903@naver.com>
2022-07-19 16:28:39 +0900

    Stack Algorithm

--- B_2000~/B_2504.py
+++ B_2000~/B_2504.py
@@ -1,3 +1,49 @@
+case=list(input())
+
+stack=[]
+
+result=0
+test=1
+
+for i in range(len(case)):
+    if case[i]=='(': # 열린 괄호는 바로바로 넣어줌
+        stack.append(case[i]) 
+        test*=2
+    elif case[i]=='[':
+        stack.append(case[i])
+        test*=3
+
+    elif case[i]==')': # 닫힌 괄호일 때
+        if not stack or stack[-1]=='[':
+            result=0
+            break
+        elif case[i-1]=='(':
+            result+=test
+            test//=2
+            stack.pop()
+        else:
+            test//=2
+            stack.pop()
+        
+    elif case[i]==']':
+        if not stack or stack[-1]=='(':
+            result=0
+            break
+        elif case[i-1]=='[':
+            result+=test
+            test//=3
+            stack.pop()
+        else:
+            test//=3
+            stack.pop()
+    
+if stack:
+    print(0)
+else:
+    print(result)
+
+# ---------------------------------------------------------------
+
 # reference
 
 T=list(input())
