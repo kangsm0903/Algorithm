ca27b9ed567dbd82a5ade2ed5d6655f5d2c622f2
kangsm0903 <kangsm0903@naver.com>
2022-07-19 18:25:28 +0900

    DFS Algorithm

--- ETC/DFS,BFS Ex.py
+++ ETC/DFS,BFS Ex.py
@@ -1,4 +1,33 @@
-# reference
+def dfs(x,y):
+    if x<0 or y<0 or x>=N or y>=M:
+        return False
+
+    # 방문 처리
+    if graph[x][y]==0:
+        graph[x][y]=1
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
+    return False
+
+N,M=map(int,input().split())
+
+graph=[]
+result=0
+
+for i in range(N):
+    graph.append(list(map(int,input())))
+
+for i in range(N):
+    for j in range(M):
+        if dfs(i,j)==True:
+            result+=1
+
+print(result)
+
+# ------------------------------------------------------------------
 
 N,M=map(int,input().split())
 
