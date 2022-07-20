52b18392c4052d6713c33582e136bb6c4ac9d688
kangsm0903 <kangsm0903@naver.com>
2022-07-20 11:13:00 +0900

    BFS Algorithm

--- ETC/DFS,BFS Ex2.py
+++ ETC/DFS,BFS Ex2.py
@@ -1,32 +1,30 @@
-# reference
-
 from collections import deque
 
-n,m=map(int,input().split())
+n,m=map(int,input().split()) # 5,6
 
 graph=[]
+
+dx=[-1,1,0,0] # 상 하
+dy=[0,0,-1,1] # 좌 우
 
 for i in range(n):
     graph.append(list(map(int,input())))
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
 def bfs(x,y):
     queue=deque([])
-    queue.append(x,y)
-    # 큐가 빌 때까지 반복하기
+    queue.append((x,y))
+
     while queue:
         x,y=queue.popleft()
-        # 현재 위치에서 4가지 방향으로의 위치 확인
+
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
-            
-            # 공간을 벗어난 경우 무시
-            if nx<0 or nx>=n or ny<0 or ny>=m:
+
+            # 범위 초과
+            if nx<0 or ny<0 or nx>=n or ny>=m:
                 continue
-            # 벽인 경우 무시
+            # 벽에 부딪혔을 때
             if graph[nx][ny]==0:
                 continue
 
