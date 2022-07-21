03b801d28f88f6131644f24199658726fd891a5a
kangsm0903 <kangsm0903@naver.com>
2022-07-21 14:46:26 +0900

    BFS Algorithm

--- B_7000~/B_7576.py
+++ B_7000~/B_7576.py
@@ -2,18 +2,14 @@ from collections import deque
 
 m,n=map(int,input().split())
 
-graph=[]
-
-for i in range(n):
-    graph.append(list(map(int,input().split())))
-# graph=[list(map(int,input().split())) for i in range(n)]
-
-queue=deque([])
-
-dx=[-1,1,0,0] # 상 하
-dy=[0,0,-1,1] # 좌 우
+graph=[list(map(int,input().split())) for i in range(n)]
 
 result=0
+
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+queue=deque([])
 
 for i in range(n):
     for j in range(m):
@@ -22,33 +18,27 @@ for i in range(n):
 
 def bfs():
     while queue:
-        # queue에 들어있는 익은 토마토의 좌표를 가져옴
         x,y=queue.popleft()
-        
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
 
-            if nx<0 or ny<0 or nx>=n or ny>=m:
-                continue
-            if graph[nx][ny]==1 or graph[nx][ny]==-1:
-                continue
-
-            if graph[nx][ny]==0:
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
                 graph[nx][ny]=graph[x][y]+1
                 queue.append((nx,ny))
 
-            # if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
-            #     graph[nx][ny]=graph[x][y]+1
-            #     queue.append((nx,ny))
-
 bfs()
 
+breaker=False
+
 for i in graph:
+    result=max(result,max(i))
     for j in i:
         if j==0:
-            print(-1)
-            exit(0)
-    result=max(result,max(i))
-# 시작을 1로 표현했으니 1을 빼줌
+            result=0
+            breaker=True
+            break
+    if breaker==True:
+        break
+
 print(result-1)
