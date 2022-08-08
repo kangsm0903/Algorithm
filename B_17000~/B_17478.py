2706767688bc76cf39bb956aa143444b7c5f7dcd
kangsm0903 <kangsm0903@naver.com>
2022-08-08 22:27:16 +0900

    Recursive Algorithm 17478

---
+++ B_17000~/B_17478.py
@@ -0,0 +1,22 @@
+N=int(input())
+
+base='____'
+
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+def recursive(n,cnt):
+    if n==0:
+        print(base*cnt+'"재귀함수가 뭔가요?"')
+        print(base*cnt+'"재귀함수는 자기 자신을 호출하는 함수라네"')
+        return
+    
+    print(base*cnt+'"재귀함수가 뭔가요?"')
+    print(base*cnt+'"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
+    print(base*cnt+'마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
+    print(base*cnt+'그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
+
+    n-=1
+    cnt+=1
+    return recursive(n,cnt) , print(base*cnt+('라고 답변하였지.'))
+
+recursive(N,0)
+print('라고 답변하였지.')
